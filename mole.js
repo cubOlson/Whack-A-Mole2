e171b7e3a6f89c8734f69084a13a01c783eafc79
cub <cub.olson@gmail.com>
2021-02-10 16:08:14 -0500

    stuck on whacked

--- mole.js
+++ mole.js
@@ -8,18 +8,31 @@ window.addEventListener('DOMContentLoaded', () => {
 //     }, 1000);
 
     function popUpRandomMole() {
-        const moleElements = document.querySelectorAll('.wgs__mole-head');
+        const moleElements = document.querySelectorAll('.wgs__mole-head:not(.wgs__mole-head--whacked)');
         const arr = Array.from(moleElements);
+        const whacked = document.querySelectorAll('.wgs__mole-head--whacked');
+        console.log(whacked.length);
+        const whackedArr = Array.from(whacked);
         const random = Math.floor(Math.random()*8);
         let current = arr[random];
         current.classList.toggle('wgs__mole-head--hidden');
         setTimeout(() => {
             hideMole(current);
-        }, 1000);
+        }, 3000);
     }
     function hideMole(current) {
         current.classList.toggle('wgs__mole-head--hidden');
         setTimeout(popUpRandomMole, 1000);
     }
     setTimeout(popUpRandomMole, 0);
+
+    const moleElements = document.querySelectorAll('.wgs__mole-head');
+    const arr = Array.from(moleElements); 
+    arr.forEach(moleHead => {
+        moleHead.addEventListener('click', event => {
+            event.target.className += ' wgs__mole-head--whacked';
+            // event.target.setAttribute('class', 'wgs__mole-head--whacked')
+            moleHead.classList.toggle('wgs__mole-head--hidden');
+        })
+    })
 });
