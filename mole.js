d6b68d29b85402938b1cfe5899d13b251c3814a9
Marshall-Diffey <marshalldiffey@yahoo.com>
2021-02-10 14:33:29 -0600

    Solved through phase 2

--- mole.js
+++ mole.js
@@ -1,4 +1,4 @@
-// window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('DOMContentLoaded', () => {
 
 //     setInterval(() => {
 //       const moleHeads = document.querySelectorAll('.wgs__mole-head');
@@ -6,5 +6,20 @@
 //         moleHead.classList.toggle('wgs__mole-head--hidden');
 //       }
 //     }, 1000);
-  
-//   });
+
+    function popUpRandomMole() {
+        const moleElements = document.querySelectorAll('.wgs__mole-head');
+        const arr = Array.from(moleElements);
+        const random = Math.floor(Math.random()*8);
+        let current = arr[random];
+        current.classList.toggle('wgs__mole-head--hidden');
+        setTimeout(() => {
+            hideMole(current);
+        }, 1000);
+    }
+    function hideMole(current) {
+        current.classList.toggle('wgs__mole-head--hidden');
+        setTimeout(popUpRandomMole, 1000);
+    }
+    setTimeout(popUpRandomMole, 0);
+});
